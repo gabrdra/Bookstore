c4bd3e2286dd3fdf4548a917bd67814549b21287
Eduardo <eduardo@pop-os.localdomain>
2021-12-06 21:33:15 -0300

    configurando ambiente da interface grafica

---
+++ src/br/ufrn/imd/application/Main.java
@@ -0,0 +1,30 @@
+package br.ufrn.imd.application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// caminho da Tela Principal
+			Parent root = FXMLLoader.load(getClass().getResource("../view/TelaPrincipal.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("BookStore");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/br/ufrn/imd/controller/TelaPrincipalController.java
@@ -0,0 +1,42 @@
+package br.ufrn.imd.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class TelaPrincipalController {
+
+    @FXML
+    private MenuItem mnItemCadProduto;
+
+    @FXML
+    private MenuItem mnItemListarProduto;
+
+    @FXML
+    private MenuItem mnItemSair;
+
+    @FXML
+    private MenuItem mnItemSobre;
+
+    @FXML
+    void abrirTelaProduto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fecharMainApp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void listarCadastroProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void listarInfo(ActionEvent event) {
+
+    }
+
+}
+
