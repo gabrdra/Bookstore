6c83b2eb0006fac09f985de2b67b12481a724517
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-12-15 18:20:20 -0300

    Adicionado exceptions para a camada de view

---
+++ src/br/ufrn/imd/exceptions/ViewException.java
@@ -0,0 +1,9 @@
+package br.ufrn.imd.exceptions;
+
+public class ViewException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ViewException() {super();}
+	public ViewException(String message) {super(message);}
+}
